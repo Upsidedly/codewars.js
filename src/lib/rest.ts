79c75c2697365a54889959ad6bab325ad855b3f7
Upsidedly <matthewlloydw@gmail.com>
2024-01-21 14:47:02 -0500

    feat: add not found safety and multi-page completed challenges

--- src/lib/rest.ts
+++ src/lib/rest.ts
@@ -1,4 +1,4 @@
-import type { User, CompletedChallenges, CodeChallenge, AuthoredChallenges, AuthoredChallenge, ClanMembers } from 'codewars-api-types/v1'
+import type { User, CompletedChallenges, CodeChallenge, AuthoredChallenges, AuthoredChallenge, ClanMembers, CompletedChallenge } from 'codewars-api-types/v1'
 
 export type APIVersion = 1
 export interface RESTOptions {
@@ -26,61 +26,115 @@ export class REST {
         this.url = `${this.api}/v${this.version}`
     }
 
-    private async request (path: string): Promise<unknown> {
-        return (await fetch(`${this.url}/${path}`)).json()
+    private async request (path: string): Promise<unknown | undefined> {
+        const result = (await fetch(`${this.url}/${path}`)).json()
+        if ("success" in result && result.success === false) return
+        return result
     }
 
-    public async user (user: string): Promise<User> {
-        return (await this.request(`/users/${user}`)) as User
+    /**
+     * Retireves a user, or undefined if the user does not exist
+     *
+     * @param user - The name of the user
+     * @returns The user
+     */
+    public async user (user: string): Promise<User | undefined> {
+        return await this.request(`/users/${user}`) as User | undefined
     }
 
-    public async completedChallenges (user: string, page: number): Promise<CompletedChallenges> {
+    /**
+     * Retrieves the completed challenges of a user, or undefined if the user does not exist
+     *
+     * @param user - The user to get the completed challenges of
+     * @param page - The page number of completed challenges to get
+     * @returns The completed challenges of the user
+     */
+    public async completedChallenges (user: string, page: number): Promise<CompletedChallenges | undefined> {
         if (!(Number.isInteger(page) && page > -1)) {
             throw new TypeError('page must a positive integer!')
         }
 
-        return (await this.request(`/users/${encodeURIComponent(user)}/code-challenges/completed?page=${page}`)) as CompletedChallenges
-    }
-
-    public async authoredChallenges (user: string): Promise<AuthoredChallenge[]> {
-        return ((await this.request(`/users/${encodeURIComponent(user)}/code-challenges/authored`)) as AuthoredChallenges).data
-    }
-
-    public async codeChallenge (challenge: string): Promise<CodeChallenge> {
-        return (await this.request(`/code-challenges/${encodeURIComponent(challenge)}`)) as CodeChallenge
+        return await this.request(`/users/${encodeURIComponent(user)}/code-challenges/completed?page=${page}`) as CompletedChallenges | undefined
     }
 
     /**
-     * Retrieves members of a clan of a certain page
+     * Retrieves every completed challenge of a user, or undefined if the user does not exist
      *
-     * @param clanName - The name of the clan
-     * @param page - The page number to retrieve 
+     * @param user - The user to get the completed challenges of
+     * @returns Every completed challenge of the user, of each page
      */
-    public async clanMembers(clanName: string, page: number): Promise<ClanMembers> {
-        if (!(Number.isInteger(page) && page > -1)) {
-            throw new TypeError('page must a positive integer!')
-        }
-
-        return (await this.request(`/clans/${encodeURIComponent(clanName)}/members?page=${page}`)) as ClanMembers
-    }
-
-    /**
-     * Retrieves every member of a clan
-     *
-     * @param clanName - The name of the clan
-     * @returns Every member of the clan, of each page
-     */
-    public async clanMembersAll(clanName: string): Promise<User[]> {
+    public async allCompletedChallenges(user: string): Promise<CompletedChallenge[] | undefined> {
         let page = 0
-        const data: User[] = []
+        const data: CompletedChallenge[] = []
         const get = async () => {
-            const members = (await this.request(`/clans/${encodeURIComponent(clanName)}/members?page=${page}`)) as ClanMembers
+            const members = await this.request(`/users/${encodeURIComponent(user)}/code-challenges/completed?page=${page}`) as CompletedChallenges
+            if (!members) return false
             data.push(...members.data)
-            if (page === members.totalPages - 1) return
+            if (page === members.totalPages - 1) return true
             page++
             await get()
         }
 
+        const result = await get()
+        if (!result) return
+        return data
+    }
+
+    /**
+     * Retrieves the authored challenges of a user, or undefined if the user does not exist
+     *
+     * @param user - The user to get the authored challenges of
+     * @returns The authored challenges of the user
+     */
+    public async authoredChallenges (user: string): Promise<AuthoredChallenge[] | undefined> {
+        const res = await this.request(`/users/${encodeURIComponent(user)}/code-challenges/authored`) as AuthoredChallenges | undefined
+        return res?.data
+    }
+
+    /**
+     * Retrieves a code challenge, or undefined if it does not exist
+     *
+     * @param challenge - The slug or id of the challenge
+     * @returns The challenge
+     */
+    public async codeChallenge (challenge: string): Promise<CodeChallenge | undefined> {
+        return await this.request(`/code-challenges/${encodeURIComponent(challenge)}`) as CodeChallenge | undefined
+    }
+
+    /**
+     * Retrieves members of a clan of a certain page, or undefined if the clan does not exist
+     *
+     * @param clanName - The name of the clan
+     * @param page - The page number to retrieve 
+     */
+    public async clanMembers(clanName: string, page: number): Promise<ClanMembers | undefined> {
+        if (!(Number.isInteger(page) && page > -1)) {
+            throw new TypeError('page must a positive integer!')
+        }
+
+        return await this.request(`/clans/${encodeURIComponent(clanName)}/members?page=${page}`) as ClanMembers | undefined
+    }
+
+    /**
+     * Retrieves every member of a clan, or undefined if the clan does not exist
+     *
+     * @param clanName - The name of the clan
+     * @returns Every member of the clan, of each page
+     */
+    public async allClanMembers(clanName: string): Promise<User[] | undefined> {
+        let page = 0
+        const data: User[] = []
+        const get = async () => {
+            const members = await this.request(`/clans/${encodeURIComponent(clanName)}/members?page=${page}`) as ClanMembers
+            if (!members) return false
+            data.push(...members.data)
+            if (page === members.totalPages - 1) return true
+            page++
+            await get()
+        }
+
+        const result = await get()
+        if (!result) return
         return data
     }
 }
